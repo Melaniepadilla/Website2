d7301c200a38cb730e06ba6c2de1fb0cb3b13d5b
Melanie Padilla <142541196+Melaniepadilla@users.noreply.github.com>
2024-04-03 17:19:38 +0000

    Wed, Apr 3, 2024, 10:19 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -9,7 +9,7 @@ score = 0
 brickRowCount = 9
 brickColumnCount = 5
 
-// Create Ball properties
+// Create ball properties
 ball = {
     x: canvas.width / 2,
     y: canvas.height / 2,
@@ -19,28 +19,27 @@ ball = {
     dy: -4,
 }
 
-
-//create paddle properties
+// Create Paddle properties
 paddle = {
     x: canvas.width / 2 - 40,
-    y: canvas.height - 20,
+    y: canvas.height -20,
     w: 80,
     h: 10,
     speed: 8,
     dx: 0,
 }
 
-//Create Brick properties
+// Create brick properties
 brickInfo = {
     w: 70,
-    h: 20,
+    h:20,
     padding: 10,
     offsetX: 45,
     offsetY: 60,
     visible: true
 }
 
-// Create Bricks
+// Create bricks
 bricks = []
 for (let i = 0; i < brickRowCount; i++) {
     bricks[i] = []
@@ -51,22 +50,24 @@ for (let i = 0; i < brickRowCount; i++) {
     }
 }
 
-//draw ball on canvas
+// Draw ball on canvas
 function drawBall() {
     ctx.beginPath()
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
-    ctx.fillStyle = '#000000'
+    ctx.fillStyle = 'pink'
     ctx.fill()
     ctx.closePath()
 }
-//draw paddle on canvas
+
+// Draw paddle on canvas
 function drawPaddle() {
     ctx.beginPath()
     ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
-    ctx.fillStyle = '#000000'
+    ctx.fillStyle = 'pink'
     ctx.fill()
     ctx.closePath()
 }
+
 
 // Draw score on canvas
 function drawScore() {
@@ -74,140 +75,144 @@ function drawScore() {
     ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
 }
 
-//draw bricks on canvas
+
+// Draw bricks on canvas
 function drawBricks() {
     bricks.forEach(column => {
         column.forEach(brick => {
             ctx.beginPath()
             ctx.rect(brick.x, brick.y, brick.w, brick.h)
-            ctx.fillStyle = brick.visible ? '#4a301f' : 'transparent';
+            ctx.fillStyle = brick.visible ? 'pink' : 'transparent';
             ctx.fill()
             ctx.closePath()
         })
     })
 }
 
-//Draw everything
+console.log(bricks)
+
+// Draw everything
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    drawBall()
     drawPaddle()
+    drawBall()
     drawScore()
     drawBricks()
 }
 
 // Move paddle on canvas
 function movePaddle() {
-    paddle.x = paddle.x + paddle.dx
+paddle.x = paddle.x + paddle.dx
 
-    // Wall Detection
+    // wall detection
     if (paddle.x < 0) {
         paddle.x = 0
     }
     if (paddle.x + paddle.w > canvas.width) {
         paddle.x = canvas.width - paddle.w
     }
+
 }
 
-//Keydown Event
+// Keydown Event
 function keyDown(e) {
-    // console.log(e.key)
-    if (e.key == 'ArrowRight' || e.key == 'Right') {
-        paddle.dx = paddle.speed
-    }
-    if (e.key == 'ArrowLeft' || e.key == 'Left') {
-        paddle.dx = -paddle.speed
-    }
+   // console.log(e.key)
+   if (e.key == 'ArrowRight' || e.key == 'Right') {
+    paddle.dx = paddle.speed
+   }
+   if (e.key == 'ArrowLeft' || e.key == 'Left') {
+    paddle.dx = -paddle.speed
+   }
 }
 
-// Keyup event
+// Keyup Event
 function keyUp(e) {
-    if (e.key == 'ArrowRight' || e.key == 'Right' || e.key == 'ArrowLeft' || e.key == 'Left') {
+    if (e.key == 'ArrowRight' ||
+     e.key == 'Right' ||
+      e.key == 'ArrowLeft' ||
+       e.key == 'Left') {
         paddle.dx = 0
     }
-
 }
 
 // Keyboard event handlers
 document.addEventListener('keydown', keyDown)
 document.addEventListener('keyup', keyUp)
 
-
-//dvd sign
 function moveBall() {
     ball.x = ball.x + ball.dx
     ball.y = ball.y + ball.dy
 
-    //Wall collision (top)
+    // Wall collision (top)
     if (ball.y + ball.size < 0) {
         ball.dy = -1 * ball.dy
     }
 
-    //wall collision right
+    // Wall collision (right)
     if (ball.x + ball.size > canvas.width) {
         ball.dx = -1 * ball.dx
     }
 
-    //wall collision bottom
+    // Wall collision (bottom)
     if (ball.y + ball.size > canvas.height) {
         ball.dy = -1 * ball.dy
         showAllBricks()
         score = 0
     }
 
-    //wall collision left
+    // Wall collision (left)
     if (ball.x + ball.size < 0) {
         ball.dx = -1 * ball.dx
     }
-
-    //paddle collision
+    // Paddle collision
     if (
-        ball.x - ball.size > paddle.x && ball.x + ball.size < paddle.x + paddle.w && ball.y + ball.size > paddle.y
+        ball.x - ball.size > paddle.x &&
+        ball.x + ball.size < paddle.x + paddle.w &&
+        ball.y + ball.size > paddle.y
     ) {
         ball.dy = -1 * ball.speed
     }
-
-    //brick collision
-    brick.forEach(column => {
-        column.forEach(brick {
+    // Brick Collision
+    bricks.forEach(column => {
+        column.forEach(brick => {
             if (brick.visible) {
                 if (
-                    ball.x - ball.size > brick.x && //left brick side
-                    ball.x + ball.size < brick.x + brick.w && //right
-                    ball.y + ball.size > brick.y && //top
-                    ball.y - ball.size < brick.y + brick.h //Bottom
+                  ball.x - ball.size > brick.x && //left
+                  ball.x + ball.size < brick.x + brick.w && //right
+                  ball.y + ball.size > brick.y && //top
+                  ball.y - ball.size < brick.y + brick.h //bottom
                 ) {
                     ball.dy = -1 * ball.dy
                     brick.visible = false
                     increaseScore()
                 }
-
             }
         })
     })
-
 }
-//Increase score
-function increaseScore() {
-    score = score + 1 //score = score + 1
 
-    if (score == brickRowCount * brickColumnCount) {
+// Increase score
+function increaseScore() {
+    score++
+
+    if (score == brickRowCount * brickRowCount) {
         score = 0
         showAllBricks()
     }
 }
 
-//Reset
 function showAllBricks() {
+    console.log("showAllBricks()")
     bricks.forEach(column => {
         column.forEach(brick => {
+            console.log("Hello")
             brick.visible = true
         })
     })
 }
 
 
-// Update the canvas drawing and animation
+// Update canvas drawing and animation
 function update() {
     moveBall()
     movePaddle()
@@ -218,11 +223,12 @@ function update() {
 update()
 
 
-// Rules open and close event handelers
+// Rules open and close event handlers
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
 })
-
 closeBtn.addEventListener('click', () => {
     rules.classList.remove('show')
 })
+
+
