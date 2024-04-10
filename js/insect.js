84aa0c23f56747684d93b23b3e7d0c3133b143ba
Melanie Padilla <142541196+Melaniepadilla@users.noreply.github.com>
2024-04-10 16:41:53 +0000

    Wed, Apr 10, 2024, 9:41 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -34,8 +34,12 @@ function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
     if (m < 10) {
-        
+        m = `0${m}`
     }
+    if (s < 10) {
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}
 }
 
 
