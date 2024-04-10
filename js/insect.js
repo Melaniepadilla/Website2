c7b483cef035b53e481da1043c96d59371730f36
Melanie Padilla <142541196+Melaniepadilla@users.noreply.github.com>
2024-04-10 15:59:51 +0000

    Wed, Apr 10, 2024, 8:59 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,5 +1,5 @@
 const screens = document.querySelectorAll('.screen')
-const choose_insect_btn = document.querySelectorAll('.choose-insect-btn')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
 const game_container = document.getElementById('game-container')
 const start_btn = document.getElementById('start-btn')
 const timeEl = document.getElementById('time')
@@ -11,4 +11,8 @@ let selected_insect = {}
 
 start_btn.addEventListener('click', () => {
     screens[0].classList.add('up')
+})
+
+choose_insect_btns.forEach(btn => {
+    
 })
