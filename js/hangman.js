f42d0f2d7ea0535a0a386ac25177f6300b8970ed
Melanie Padilla <142541196+Melaniepadilla@users.noreply.github.com>
2024-04-22 16:50:20 +0000

    Mon, Apr 22, 2024, 9:50 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -8,4 +8,7 @@ const figureParts = document.querySelectorAll('figure-part')
 
 const word = ['application', 'programming', 'interface', 'wizard']
 
-let selectedIndex = 
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
+
+console.log(selectedWord)
